ae4fdba25c57f76859ed7e11e1adae8753ca79d9
Yamini Patta <yamini.pattan@gmail.com>
2019-04-17 10:33:05 -0700

    added simple button functionality: text at top and on button changes

--- app/src/main/java/com/example/walkthedog/MainActivity.java
+++ app/src/main/java/com/example/walkthedog/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.walkthedog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int x = 0;
+    public void walking(View view) {
+        x++;
+        if (x%2 == 0) {
+            started();
+        }
+        else {
+            ended();
+        }
+    }
+
+    public void started() {
+        String a = "You're on a walk!";
+        String b = "I'm Done";
+        display(a, b);
+    }
+
+    public void ended() {
+        String a = "You've finished your walk!";
+        String b = "Let's Go!";
+        display(a, b);
+    }
+
+    public void display(String what, String whatnow) {
+        TextView walkTextView = findViewById(R.id.walk_time_text);
+        walkTextView.setText(what);
+        TextView buttonTextView = findViewById(R.id.lets_go_button);
+        buttonTextView.setText(whatnow);
     }
 }
